f63d09369bf1d68fdd5316ac03269231ed945584
hibaasim <hiba.asim10@gmail.com>
2023-12-17 18:22:11 -0800

    exec fix

--- exec.c
+++ exec.c
@@ -13,16 +13,18 @@ globe_s globe = {NULL, NULL};
 int exec(stack_t **head, unsigned int line_num, char *input, FILE *file)
 {
 	instruction_t opera[] = {
-								{"push", f_push}, {"pall", f_pall},
-								{"pint", f_pint}, {"pop", f_pop},
-								{"swap", f_swap}, {"add", f_add},
-								{"nop", f_nop}, {NULL, NULL}};
+					{"push", f_push}, {"pall", f_pall},
+					{"pint", f_pint}, {"pop", f_pop},
+					{"swap", f_swap}, {"add", f_add},
+					{"nop", f_nop}, {NULL, NULL}};
 	char *commd, *value;
 	int i = 0;
 
 	globe.file = file;
 	/*globe.input = input;*/
 	commd = strtok(input, " \n\t$");
+	if (commd && commd[0] == '#')
+		return (0);
 	value = strtok(NULL, " \n\t");
 	globe.args = value;
 	while (opera[i].opcode && commd)
